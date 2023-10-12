0d14c963d9d5e1e8bb2f781c4a8ded51f6d69826
brian nelson <bn3l10@gmail.com>
2023-10-12 07:33:42 -0400

    browser

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 // import { AnimatePresence } from 'framer-motion';
 import Home from './pages/home';
 import MenuBreakfast from './pages/menuBreakfast';
--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const NotFound = () => {
     return (
-    <div className="font-mentra text-[3rem] tracking-widest text-center m-[3rem] pt-8">404</div>
+    <div className="font-gt uppercase text-[3rem] tracking-widest text-center m-[3rem] pt-8">404</div>
     );
 };
 
