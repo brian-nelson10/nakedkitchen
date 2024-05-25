3bc754d86aed44fd60da610148c25a9deda86ef9
brian nelson <bn3l10@gmail.com>
2024-05-25 10:27:57 -0400

    update

--- src/components/MenuHero/porch.js
+++ src/components/MenuHero/porch.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { motion } from 'framer-motion';
-import Button from "../Button/button";
+// import Button from "../Button/button";
 
 const PorchHero = () => {
     return (
--- src/pages/merch.js
+++ src/pages/merch.js
@@ -1,7 +1,6 @@
-
-
-
-import React from "react";
+import React, { useState } from "react";
+import { Modal } from "react-responsive-modal";
+import "react-responsive-modal/styles.css";
 import "./home.css";
 import Footer from "../components/Footer";
 import Navbar from "../components/Navbar";
@@ -14,8 +13,22 @@ import shirt1 from "../assets/images/lineCookBlackFront.jpg";
 import shirt2 from "../assets/images/linecookteeclouds2.jpeg";
 import clem2 from "../assets/images/clemFront.jpg";
 import clem from "../assets/images/clemBack.jpg";
+import mustardback from "../assets/images/mustardback.jpg";
+import mustardfront from "../assets/images/mustardfront.jpg";
+
 // import MerchCarousel from "../components/Carousel/merchCarousel";
 const Merch = () => {
+    const [open, setOpen] = useState(false);
+    const [selectedImage, setSelectedImage] = useState("");
+
+    const onOpenModal = (image) => {
+        setSelectedImage(image);
+        setOpen(true);
+    };
+
+    const onCloseModal = () => {
+        setOpen(false);
+    };
 
     return (
         <main className="bg-[#dbe7e8]">
@@ -29,40 +42,65 @@ const Merch = () => {
                     </section>
                     <section className="relative h-fit overflow-hidden bg-[#020202] merch">
                         <div className="text-[#e3b505] font-poppins text-center mt-[4rem] bg-black mx-[42rem] py-10 rounded bg-opacity-75">
-                            <p className="mb-2">Tax & Shipping Included!</p>
-                            <p className="mb-2">All proceeds go into opening our restaurant!</p>
+                            <p className="mb-2">Shipping Included!</p>
+                            <p className="mb-2">All proceeds go into art for our restaurant!</p>
                         </div>
                         <div className="grid grid-rows-2 p-[3rem] m-[3rem] justify-center items-center text-center gap-4">
                             <div className="border-3 border border-black p-10 bg-black bg-opacity-95">
-                                <div className="mb-2 text-[#47b9c3] font-gt uppercase text-[3rem]">The Line Cook Tee</div>
-                                <div className="mb-2 text-white md:mx-10 mx-[3rem] md:px-10 mb-5 font-poppins uppercase md:text-[1.5rem]">Small Left Chest Logo with Large Back Print on Comfort Colors Tee</div>
-                                <div className="w-fit md:mx-[32rem] ">
-                                    <img className="md:h-[30rem] md:w-[30rem] my-1 border-[.2rem] border border-white" src={shirt} />
-                                    <img className="md:h-[30rem] md:w-[30rem] my-1 border-[.2rem] border border-white" src={shirt1} />
-                                    <img className="md:h-[30rem] md:w-[30rem] my-1 border-[.2rem] border border-white" src={shirt2} />
+                                <div className="mb-2 text-[#47b9c3] font-gt uppercase text-[3rem]">Mustard Eye Tee</div>
+                                <div className="mb-2 text-white md:mx-10 mx-[3rem] md:px-10 mb-5 font-poppins uppercase md:text-[1.5rem]">Small Left Chest Logo with Large Back Print on Bella & Canvas Tee</div>
+                                <div className="w-fit flex flex-col md:flex-row mx-[4rem] ">
+                                    <img className="md:h-[30rem] md:w-[30rem] my-1 border-[.2rem] border border-white cursor-pointer" src={mustardback} onClick={() => onOpenModal(mustardback)} />
+                                    <img className="md:h-[30rem] md:w-[30rem] my-1 border-[.2rem] border border-white cursor-pointer" src={mustardfront} onClick={() => onOpenModal(mustardfront)} />
+                                    <img className="md:h-[30rem] md:w-[30rem] my-1 border-[.2rem] border border-white cursor-pointer" src={mustardback} onClick={() => onOpenModal(mustardback)} />
                                 </div>
-                                <div className="mt-[8rem]">
-                                <stripe-buy-button
-                                    buy-button-id="buy_btn_1O6YVRB3OInBaAUnlO4K7IMt"
-                                    publishable-key="pk_live_51NOKTIB3OInBaAUnvrJx5QEK7UKLkB1sRz2R9vixw3X7k0rxLGHmn4XT370M2NOtuQXsikfozTt2GnYteNfUjwbQ00lk56yFck"
-                                >
-                                </stripe-buy-button>
+                                <div className="my-[2rem]">
+                                    <div className="text-[#47b9c3] font-gt uppercase text-[4rem]">
+                                        <p className="my-2">20$</p>
+                                    </div>
+                                    <stripe-buy-button
+                                        buy-button-id="buy_btn_1PKLPfB3OInBaAUn3ZyarQ0j"
+                                        publishable-key="pk_live_51NOKTIB3OInBaAUnvrJx5QEK7UKLkB1sRz2R9vixw3X7k0rxLGHmn4XT370M2NOtuQXsikfozTt2GnYteNfUjwbQ00lk56yFck"
+                                    >
+                                    </stripe-buy-button>
                                 </div>
                             </div>
                             <div className="border-3 border border-black p-10 bg-black bg-opacity-95">
                                 <div className="mb-2 text-[#47b9c3] font-gt uppercase text-[3rem]">Clementine Tee</div>
-                                <div className="mb-2 text-white md:mx-10 md:px-10 mb-5 font-poppins uppercase md:text-[1.5rem]">Oversized Back Print With left chest logo print on Comfort Colors Tee</div>
+                                <div className="mb-2 text-white md:mx-10 md:px-10 mb-5 font-poppins uppercase md:text-[1.5rem]">Oversized Back Print With left chest logo print on Bella & Canvas Tee</div>
                                 <div className="w-fit flex flex-col md:flex-row mx-[4rem]">
-                                    <img className="md:h-[30rem] md:w-[30rem] my-1 border-[.2rem] border border-[#e3b505] " src={clem} />
-                                    <img className="md:h-[30rem] md:w-[30rem] my-1 border-[.2rem] border border-[#e3b505] hidden md:block" src={clem2} />
-                                    <img className="md:h-[30rem] md:w-[30rem] my-1 border-[.2rem] border border-[#e3b505] hidden md:block" src={clem} />
+                                    <img className="md:h-[30rem] md:w-[30rem] my-1 border-[.2rem] border border-[#e3b505] cursor-pointer" src={clem} onClick={() => onOpenModal(clem)} />
+                                    <img className="md:h-[30rem] md:w-[30rem] my-1 border-[.2rem] border border-[#e3b505] cursor-pointer" src={clem2} onClick={() => onOpenModal(clem2)} />
+                                    <img className="md:h-[30rem] md:w-[30rem] my-1 border-[.2rem] border border-[#e3b505] cursor-pointer" src={clem} onClick={() => onOpenModal(clem)} />
                                 </div>
-                                <div className="mt-8">
-                                <stripe-buy-button
-                                    buy-button-id="buy_btn_1O6ccMB3OInBaAUnRPG3hZ9b"
-                                    publishable-key="pk_live_51NOKTIB3OInBaAUnvrJx5QEK7UKLkB1sRz2R9vixw3X7k0rxLGHmn4XT370M2NOtuQXsikfozTt2GnYteNfUjwbQ00lk56yFck"
-                                >
-                                </stripe-buy-button>
+                                <div className="my-[2rem]">
+                                    <div className="text-[#47b9c3] font-gt uppercase text-[4rem]">
+                                        <p className="my-2">20$</p>
+                                    </div>
+                                    <stripe-buy-button
+                                        buy-button-id="buy_btn_1PKLEuB3OInBaAUnJDVPWfd1"
+                                        publishable-key="pk_live_51NOKTIB3OInBaAUnvrJx5QEK7UKLkB1sRz2R9vixw3X7k0rxLGHmn4XT370M2NOtuQXsikfozTt2GnYteNfUjwbQ00lk56yFck"
+                                    >
+                                    </stripe-buy-button>
+                                </div>
+                            </div>
+                            <div className="border-3 border border-black p-10 bg-black bg-opacity-95">
+                                <div className="mb-2 text-[#47b9c3] font-gt uppercase text-[3rem]">The Line Cook Tee</div>
+                                <div className="mb-2 text-white md:mx-10 mx-[3rem] md:px-10 mb-5 font-poppins uppercase md:text-[1.5rem]">Small Left Chest Logo with Large Back Print on American Apparel Tee</div>
+                                <div className="w-fit flex flex-col md:flex-row mx-[4rem] ">
+                                    <img className="md:h-[30rem] md:w-[30rem] my-1 border-[.2rem] border border-white cursor-pointer" src={shirt} onClick={() => onOpenModal(shirt)} />
+                                    <img className="md:h-[30rem] md:w-[30rem] my-1 border-[.2rem] border border-white cursor-pointer" src={shirt1} onClick={() => onOpenModal(shirt1)} />
+                                    <img className="md:h-[30rem] md:w-[30rem] my-1 border-[.2rem] border border-white cursor-pointer" src={shirt2} onClick={() => onOpenModal(shirt2)} />
+                                </div>
+                                <div className="my-[2rem]">
+                                    <div className="text-[#47b9c3] font-gt uppercase text-[4rem]">
+                                        <p className="my-2">20$</p>
+                                    </div>
+                                    <stripe-buy-button
+                                        buy-button-id="buy_btn_1PKLWxB3OInBaAUnRpZFl4hs"
+                                        publishable-key="pk_live_51NOKTIB3OInBaAUnvrJx5QEK7UKLkB1sRz2R9vixw3X7k0rxLGHmn4XT370M2NOtuQXsikfozTt2GnYteNfUjwbQ00lk56yFck"
+                                    >
+                                    </stripe-buy-button>
                                 </div>
                             </div>
                         </div>
@@ -81,12 +119,11 @@ const Merch = () => {
                     </section>
                 </div>
             </div>
+            <Modal open={open} onClose={onCloseModal} center>
+                <img src={selectedImage} alt="Popup" className="w-full h-full" />
+            </Modal>
         </main>
-    )
+    );
 };
 
 export default Merch;
-
-
-
-
