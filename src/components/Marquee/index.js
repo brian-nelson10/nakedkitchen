4ed4afd8ae8972d149951fad8d01299d84b8b516
brian nelson <bn3l10@gmail.com>
2024-06-01 18:03:46 -0400

    update

--- src/components/Marquee/index.js
+++ src/components/Marquee/index.js
@@ -6,12 +6,12 @@ import { motion } from "framer-motion";
 // 2. Defining Variants
 const marqueeVariants = {
   animate: {
-    x: [-300, -1050],
+    x: [-200, -1000],
     transition: {
       x: {
         repeat: Infinity,
         repeatType: "loop",
-        duration: 100,
+        duration: 90,
         ease: "linear",
       },
     },
@@ -21,14 +21,14 @@ const marqueeVariants = {
 const Marquee = () => {
   return (
     <div> 
-      <div className="marquee relative md:overflow-hidden h-[210px]" >
+      <div className="marquee relative overflow-hidden h-[210px]" >
         <motion.div
-          className="track md:overflow-hidden"
+          className="track overflow-hidden"
           variants={marqueeVariants}
           animate="animate"
         >
-          <div className="font-gt text-[7rem] md:text-[9rem] drop-shadow-[6px_6px_0px_#e3b505] md:drop-shadow-[4px_4px_0px_#e3b505] stroke uppercase text-[#F6B092]">
-            Our Menu. Our Menu. 
+          <div className="overflow-hidden font-gt text-[5rem] md:text-[9rem] drop-shadow-[6px_6px_0px_#e3b505] md:drop-shadow-[4px_4px_0px_#e3b505] stroke uppercase text-[#F6B092]">
+            Our Menu. Our Menu. Our Menu. Our Menu.
           </div>
         </motion.div>
       </div>
