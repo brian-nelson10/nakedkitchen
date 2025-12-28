fb3808fc093dec4a1e2fa066b1c58d4aeb98822d
brian nelson <bn3l10@gmail.com>
2025-12-27 22:52:52 -0500

    update

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -4,9 +4,9 @@ import logo from "../assets/images/logo8.png"
 const NotFound = () => {
     return (
     <>
-    <div className="flex flex-col justify-center items-center text-center ">
-    <div className="font-gt uppercase text-[3rem] tracking-widest text-center m-[3rem] pt-8">Coming Soon..</div>
-    <img className="w-[8rem] h-[8rem] " src={logo} />
+    <div className="flex flex-col justify-center items-center text-center h-screen w-screen grass">
+    <div className="font-gt uppercase text-[3rem] tracking-widest text-center m-[3rem] pt-8">Coming Soon.. like.. Soon-Soon.</div>
+    <img className="w-[8rem] h-[8rem] " alt="naked kitchen logo yo" src={logo} />
     </div>
     </>
     );
